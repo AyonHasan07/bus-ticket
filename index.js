1fa0024a05fd7a4bc720cf1e416573ff0e64ecd4
AyonHasan07 <rh.rakibulhasan77@gmail.com>
2024-02-19 04:27:56 +0600

    working on coupon section

--- index.js
+++ index.js
@@ -22,10 +22,43 @@ function seatBooking(element){
     
     totalPrice = totalPrice + 550;
     total(totalPrice);
+    // console.log(totalPrice);
 
-    console.log(totalPrice);
+    
 
 }
+
+document.getElementById('couponSubmit').addEventListener('click',finalBalance);
+
+function finalBalance(){
+    const coupon = document.getElementById('couponInput').value;
+    const upperCoupon = coupon.toUpperCase();
+    
+    // console.log(upperCoupon);
+    if( upperCoupon === 'NEW15'){
+        const discount = totalPrice * .15 ;
+        const grandPrice = totalPrice -discount;
+        new15(grandPrice);
+        discountText(discount);
+        hideThings();
+
+    }
+    // else if(upperCoupon === 'couple20'){
+    //     const discount = totalPrice * .20 ;
+    //     const grandPrice = totalPrice -discount;
+    //     new15(grandPrice);
+    //     discountText(discount);
+    //     hideThings();
+    // }
+
+}
+
+function new15(element){
+    const grandTotal = document.getElementById('grandTotal');
+    grandTotal.innerText = element;
+}
+
+
 
 function setColorById(element){
     const takeElement = document.getElementById(element);
@@ -57,7 +90,7 @@ function addElement(seatNumber){
 }
 
 function seatLeft(element){
-    seatLeftNow = document.getElementById('seatLeft');
+   const seatLeftNow = document.getElementById('seatLeft');
     seatLeftNow.innerText = element;
 }
 
@@ -65,3 +98,31 @@ function total(element){
     const totalAmaount = document.getElementById('totalPrice');
     totalAmaount.innerText = element;
 }
+
+function discountText(element){
+    const findDiv = document.getElementById('discount-container');
+
+    const newTable = document.createElement('table');
+    newTable.style.width = '100%';
+    newTable.style.textAlign = 'center';
+
+    const tableRow = document.createElement('tr');
+
+    const tableData = document.createElement('td'); 
+    tableData.innerText = 'Discount';
+
+    const tableData2 = document.createElement('td');
+    tableData2.innerText = element
+
+    newTable.appendChild(tableRow);
+    tableRow.appendChild(tableData);
+    tableRow.appendChild(tableData2);
+    findDiv.appendChild(newTable);
+}
+
+function hideThings(){
+    const hide = document.getElementById('couponDiv');
+    hide.setAttribute('class', 'hidden');
+
+}
+
